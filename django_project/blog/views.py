da6697e21e85974ffcbea131b781ec876c5c9062
unknown <79578683+Treszyk@users.noreply.github.com>
2024-07-25 18:46:32 +0200

    Added base, home, about templates and added CSS

--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -1,8 +1,25 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+posts = [
+    {
+        'author': 'Adam',
+        'title': 'Blog Post 1',
+        'content': 'First post content',
+        'date_posted': 'July 25, 2024'
+    },
+    {
+        'author': 'Patryk',
+        'title': 'Blog Post 2',
+        'content': 'Second post content',
+        'date_posted': 'June 25, 2024'
+    }
+]
 def home(request):
-    return HttpResponse('<h1>Blog Home</h1>')
+    context = {
+        'posts': posts
+    }
+    return render(request, 'blog/home.html', context)
 
 def about(request):
-    return HttpResponse('<h1>Blog About</h1>')
+    return render(request, 'blog/about.html', {'title': 'About'})
