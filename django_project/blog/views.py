f00daa1099503316e37b6fd554fe86ddd0cdf60e
unknown <79578683+Treszyk@users.noreply.github.com>
2024-07-26 15:39:03 +0200

    Added a Post model

--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -1,23 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Post
 
-posts = [
-    {
-        'author': 'Adam',
-        'title': 'Blog Post 1',
-        'content': 'First post content',
-        'date_posted': 'July 25, 2024'
-    },
-    {
-        'author': 'Patryk',
-        'title': 'Blog Post 2',
-        'content': 'Second post content',
-        'date_posted': 'June 25, 2024'
-    }
-]
 def home(request):
     context = {
-        'posts': posts
+        'posts': Post.objects.all()
     }
     return render(request, 'blog/home.html', context)
 
